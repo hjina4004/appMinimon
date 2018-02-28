7bf1c13be1f1ee235a7e3501477314bb0534f260
yhYoon <dbsdugus00@gmail.com>
2018-02-28 10:46:21 +0900

    설정 바 기본틀

---
+++ app/src/main/java/com/minimon/diocian/player/VerticalSeekBar.java
@@ -0,0 +1,54 @@
+package com.example.good.gestureexample;
+
+import android.content.Context;
+import android.graphics.Canvas;
+import android.util.AttributeSet;
+import android.view.MotionEvent;
+import android.widget.SeekBar;
+
+/**
+ * Created by GOOD on 2018-02-26.
+ */
+
+public class VerticalSeekBar extends android.support.v7.widget.AppCompatSeekBar {
+    public VerticalSeekBar(Context context) {
+        super(context);
+    }
+
+    public VerticalSeekBar(Context context, AttributeSet attrs) {
+        super(context, attrs);
+    }
+
+    public VerticalSeekBar(Context context, AttributeSet attrs, int defStyleAttr) {
+        super(context, attrs, defStyleAttr);
+    }
+
+    @Override
+    protected void onSizeChanged(int w, int h, int oldw, int oldh) {
+        super.onSizeChanged(h, w, oldh, oldw);
+    }
+
+    @Override
+    protected synchronized void onMeasure(int widthMeasureSpec, int heightMeasureSpec) {
+        super.onMeasure(widthMeasureSpec, heightMeasureSpec);
+        setMeasuredDimension(getMeasuredHeight(),getMeasuredWidth());
+    }
+
+    @Override
+    protected synchronized void onDraw(Canvas canvas) {
+        canvas.rotate(-90);
+        canvas.translate(-getHeight(),0);
+        super.onDraw(canvas);
+    }
+
+    @Override
+    public boolean onTouchEvent(MotionEvent event) {
+        if(!isEnabled()){
+            return false;
+        }
+        switch (event.getAction()){
+
+        }
+        return super.onTouchEvent(event);
+    }
+}
