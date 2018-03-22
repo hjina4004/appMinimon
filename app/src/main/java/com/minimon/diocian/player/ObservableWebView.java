0b176704ccd4deb4494d5128141cf85d9defd51c
jina <hjina4004@gmail.com>
2018-03-22 12:05:26 +0900

    WebView Touch event

--- app/src/main/java/com/minimon/diocian/player/ObservableWebView.java
+++ app/src/main/java/com/minimon/diocian/player/ObservableWebView.java
@@ -2,6 +2,9 @@ package com.minimon.diocian.player;
 
 import android.content.Context;
 import android.util.AttributeSet;
+import android.util.Log;
+import android.view.GestureDetector;
+import android.view.MotionEvent;
 import android.webkit.WebView;
 
 /**
@@ -12,22 +15,67 @@ public class ObservableWebView extends WebView {
 
     private OnScrollChangedCallback mOnScrollChangedCallback;
 
+    private Context context;
+    private GestureDetector gestDetector;
+
     public ObservableWebView(Context context) {
         super(context);
+        init(context);
     }
 
     public ObservableWebView(Context context, AttributeSet attrs) {
         super(context, attrs);
+        init(context);
     }
 
     public ObservableWebView(Context context, AttributeSet attrs, int defStyleAttr) {
         super(context, attrs, defStyleAttr);
+        init(context);
     }
+
+    private void init(Context context) {
+        this.context = context;
+        gestDetector = new GestureDetector(context, gestListener);
+    }
+
+    GestureDetector.SimpleOnGestureListener gestListener= new GestureDetector.SimpleOnGestureListener() {
+        public boolean onDown(MotionEvent event) {
+            return true;
+        }
+
+        public boolean onFling(MotionEvent event1, MotionEvent event2, float velocityX, float velocityY) {
+            if (event1.getRawY() > event2.getRawY()) {
+                Log.i("WebView", "[swipe] up");
+            } else {
+                Log.i("WebView", "[swipe] down");
+            }
+            //you can trace any touch events here
+            return true;
+        }
+    };
 
     @Override
     protected void onScrollChanged(int l, int t, int oldl, int oldt) {
+        Log.i("WebView", "[onScrollChanged]");
         super.onScrollChanged(l, t, oldl, oldt);
         if(mOnScrollChangedCallback != null) mOnScrollChangedCallback.onScroll(l, t, oldl, oldt);
+    }
+
+    @Override
+    public boolean onTouchEvent(MotionEvent event){
+        //Log.i("WebView", "[onTouchEvent]");
+        super.onTouchEvent(event);
+
+        int action = event.getAction();
+        if (action == MotionEvent.ACTION_DOWN) {
+            Log.d("WebView", "OnTouch : ACTION_DOWN");
+        } else if(action == MotionEvent.ACTION_UP){
+            Log.d("WebView", "OnTouch : ACTION_UP");
+        } else if(action == MotionEvent.ACTION_MOVE){
+            Log.d("WebView", "OnTouch : ACTION_MOVE");
+        }
+
+        return gestDetector.onTouchEvent(event);
     }
 
     public OnScrollChangedCallback getOnScrollChangedCallback()
